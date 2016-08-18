f60478e96cf2d1d284baa036712b76f7c7ab5cd5
lukelliot <lucaselliotchin@gmail.com>
2016-08-18 16:30:14 -0400

    updated new file paths for entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  entry: "./lib/entry.js",
+  entry: "./lib/js/main.js",
   output: {
-    path: path.join(__dirname, lib),
-  	filename: "bundle.js"
+  	filename: "./lib/bundle.js"
   },
   module: {
     loaders: [
