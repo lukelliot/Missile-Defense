65b2f96c8e9e81d9d2c373a3953f876b3415397f
lukelliot <lucaselliotchin@gmail.com>
2016-08-18 14:20:02 -0400

    set defaults

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "./lib/entry.js",
+  output: {
+    path: path.join(__dirname, lib),
+  	filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
