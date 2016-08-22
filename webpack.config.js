ae277f4574b7181ee618abd031870d4233fb964a
lukelliot <lucaselliotchin@gmail.com>
2016-08-22 11:21:32 -0400

    removed react preset, unneeded

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015']
         }
       }
     ]
